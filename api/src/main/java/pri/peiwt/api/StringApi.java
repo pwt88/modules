8b7b7f03061449ab1dd199aaec6ecc4484a56976
pwt88@163.com <git12354>
2020-01-15 18:27:02 +0800

    首次导入

---
+++ api/src/main/java/pri/peiwt/api/StringApi.java
@@ -0,0 +1,14 @@
+package pri.peiwt.api;
+
+import pri.peiwt.util.StringUtil;
+
+/**
+ * @author PeiWentao
+ */
+public class StringApi {
+
+    public static void main(String[] args) {
+        String str = StringUtil.int2Str(2);
+        System.out.println(str);
+    }
+}
---
+++ util/src/main/java/pri/peiwt/util/StringUtil.java
@@ -0,0 +1,10 @@
+package pri.peiwt.util;
+
+/**
+ * @author PeiWentao
+ */
+public class StringUtil {
+    public static String int2Str(int i) {
+        return String.valueOf(i);
+    }
+}
