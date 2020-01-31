c2f7bcdb7f4a63547d694103fe855a097f0c07c8
pwt88@163.com <git12354>
2020-01-31 23:20:57 +0800

    新增角色类

---
+++ api/src/main/java/pri/peiwt/model/Role.java
@@ -0,0 +1,8 @@
+package pri.peiwt.model;
+
+/**
+ * @author PeiWentao
+ * @date 2020/1/31 - 23:19
+ */
+public class Role {
+}
--- api/src/main/java/pri/peiwt/model/User.java
+++ api/src/main/java/pri/peiwt/model/User.java
@@ -4,5 +4,23 @@ package pri.peiwt.model;
  * @author PeiWentao
  * @date 2020/1/15 - 18:30
  */
-public class User {123
+public class User {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
